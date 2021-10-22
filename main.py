f1873bc00d575ec25f9b79f716f0e60a66ae9005
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-10-22 08:43:34 +0200

    feat(page): Add default page

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/<name>")
+def hello(name):
+    return f"Sorry, the page {escape(name)} do not exist!"
