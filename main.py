09023ae85d514c5aed53cfb7ab91cac54457075f
Amaleo83 <92971634+Amaleo83@users.noreply.github.com>
2021-10-22 10:42:45 +0200

    BDD

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import con as con
 from flask import Flask
 from markupsafe import escape
 import sqlite3
+from flask import current_app, g
 
 app = Flask(__name__)
 
@@ -17,3 +19,21 @@ def hello(name):
 
 conn = sqlite3.connect('flaskProject.db')
 print("Opened database successfully")
+
+cur = con.cursor()
+
+
+def get_db():
+    if 'flaskProject' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['flaskProject'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
