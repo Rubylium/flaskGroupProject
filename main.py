8057de53f773c06f8158b225a434706af82d9b44
CPerrichon <62330243+CPerrichon@users.noreply.github.com>
2021-10-22 15:09:03 +0200

    Page de connexion et route connexion

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import bp as bp
 import click
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, flash, url_for
 from flask.cli import with_appcontext
 from markupsafe import escape
 import sqlite3
 from flask import current_app, g
+from werkzeug.security import check_password_hash
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 # Routes
+
 
 @app.route("/")
 def index():
@@ -19,12 +23,38 @@ def hello(name):
     return f"Sorry, the page {escape(name)} do not exist!"
 
 
+@app.route("/", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        user = request.form["user"]
+        password = request.form["password"]
+        db = get_db_connection()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (user,)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
 # DB Connection + creation of tables
 conn = sqlite3.connect("flaskProject.db")
 print("Opened database successfully")
 cursor = conn.cursor()
 
-sql_file=open("schema.sql")
+sql_file = open("schema.sql")
 sql_as_string = sql_file.read()
 cursor.executescript(sql_as_string)
 
@@ -42,6 +72,3 @@ def close_db(e=None):
     db = g.pop("db", None)
     if db is not None:
         db.close()
-
-
-
